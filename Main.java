31bdd57c153c408a4a0193bdceeffe2b24ae1aee
AhmedAlharbi15 <159686025+AhmedAlharbi15@users.noreply.github.com>
2024-02-26 14:15:09 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        Scanner in = new Scanner(System.in);
+//       q1:
+//        System.out.println("Number 1");
+//        int num1 = in.nextInt();
+//        System.out.println("Number2");
+//        int num2 = in.nextInt();
+//        System.out.println("Number3");
+//        int num3 = in.nextInt();
+//        if (num1 > num2 && num1 > num3) {
+//            System.out.println("biggest number " +num1);
+//        } else if (num2 > num1 && num2 > num3){
+//            System.out.println("biggest number "+num2);
+//        }else {
+//            System.out.println("biggest number "+num3);
+//        }
+
+
+//      q2:
+//      System.out.println("Name 1");
+//        String name1=in.nextLine();
+//        System.out.println("Name 2");
+//        String name2=in.nextLine();
+//        System.out.println("Name 3");
+//        String name3=in.nextLine();
+//
+//        System.out.println(name1+" "+name1.length());
+//        System.out.println(name2+" "+name2.length());
+//        System.out.println(name3+" "+name3.length());
+
+//       Q3:
+//        System.out.println("Please number:");
+//        int na1= in.nextInt();
+//        System.out.println("Please index");
+//        int ind1= in.n();
+
+
+
+
+//
+//        q4:
+        System.out.println("Please enter a string :");
+        String s1= in.nextLine();
+        char c;
+        if ()
+
+
+    }
+}
